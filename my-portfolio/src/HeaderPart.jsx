1a78e1e1505da2db45be3e73570396e89e34a4f0
Skoco Pavle <skocopavle@gmail.com>
2025-07-05 22:20:59 +0200

    Added all the buttons, theri positions and colored thar borders

--- my-portfolio/src/HeaderPart.jsx
+++ my-portfolio/src/HeaderPart.jsx
@@ -10,19 +10,23 @@ export function Header () {
                 </div>
                 <h1>Full-Stack Web Developer</h1>
                 <hr></hr>
-                <div className="buttons">
-                    <button>Web development</button>
-                    <button>Web Applications</button>
-                    <button>Web Design</button>
-                    <button>Full-Stack Development</button>
-                    <button>Front-End Development</button>
-                    <button>Back-End Development</button>
-                    <button>Front-End Frameworks</button>
-                    <button>Back-End Frameworks</button>
-                    <button>Computer Science</button>
-                    <button>Software Development</button>
-                    <button>Algorithms</button>
-                    <button>Algorithm Development</button>
+                <div className="buttons-top">
+                    <button className="btn">Web development</button>
+                    <button className="btn">Web Applications</button>
+                    <button className="btn">Web Design</button>
+                    <button className="btn">Full-Stack Development</button>
+                    <button className="btn">Front-End Development</button>
+                    <button className="btn">Back-End Development</button>
+                </div>
+                <div className="buttons-middle">
+                    <button className="btn">Front-End Frameworks</button>
+                    <button className="btn">Back-End Frameworks</button>
+                </div>
+                <div className="buttons-bottom">
+                    <button className="btn">Computer Science</button>
+                    <button className="btn">Software Development</button>
+                    <button className="btn">Algorithms</button>
+                    <button className="btn">Algorithm Development</button>
                 </div>
             </header>
         </>
