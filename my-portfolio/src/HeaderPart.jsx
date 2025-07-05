924f9064af05741b099240cf73b77a6458930ca0
Skoco Pavle <skocopavle@gmail.com>
2025-07-05 14:20:53 +0200

    Added few buttons to the headear and the hr line

--- my-portfolio/src/HeaderPart.jsx
+++ my-portfolio/src/HeaderPart.jsx
@@ -10,6 +10,20 @@ export function Header () {
                 </div>
                 <h1>Full-Stack Web Developer</h1>
                 <hr></hr>
+                <div className="buttons">
+                    <button>Web development</button>
+                    <button>Web Applications</button>
+                    <button>Web Design</button>
+                    <button>Full-Stack Development</button>
+                    <button>Front-End Development</button>
+                    <button>Back-End Development</button>
+                    <button>Front-End Frameworks</button>
+                    <button>Back-End Frameworks</button>
+                    <button>Computer Science</button>
+                    <button>Software Development</button>
+                    <button>Algorithms</button>
+                    <button>Algorithm Development</button>
+                </div>
             </header>
         </>
     )
