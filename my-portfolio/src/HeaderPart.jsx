fe4242461537dc73544f3e67a8f2a3df0a697145
Skoco Pavle <skocopavle@gmail.com>
2025-07-07 17:30:51 +0200

    Added final details to my header

--- my-portfolio/src/HeaderPart.jsx
+++ my-portfolio/src/HeaderPart.jsx
@@ -27,7 +27,6 @@ export function Header () {
                     <button className="btn">Software Development</button>
                     <button className="btn">Algorithms</button>
                     <button className="btn">Algorithm Development</button>
-
                 </div>
             </header>
         </>
