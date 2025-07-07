2aa383ef264b02896af85065166493624c121703
Skoco Pavle <skocopavle@gmail.com>
2025-07-07 20:26:13 +0200

    Added animations to my header section

--- my-portfolio/src/HeaderPart.jsx
+++ my-portfolio/src/HeaderPart.jsx
@@ -1,16 +1,24 @@
+import { useEffect, useState } from "react";
 import "./HeaderPart.css";
 import myImage from "./img/moja-slika2.jpg";
 
+
 export function Header () {
+    const [animate, setAnimate] = useState(false)
+
+    useEffect(() => {
+        setAnimate(true)
+    }, [])
+
     return (
         <>
             <header>
-                <div className="my-picture">
+                <div className={`my-picture ${animate ? "animate" : ""}`}>
                     <img src={myImage} alt="My picture"className="pic"/>
                 </div>
-                <h1>Full-Stack Web Developer</h1>
-                <hr></hr>
-                <div className="buttons-top">
+                <h1 className={animate ? "animate" : ""}>Full-Stack Developer</h1>
+                <hr className={animate ? "animate" : ""}></hr>
+                <div className={`buttons-top ${animate ? "animate" : ""}`}>
                     <button className="btn">Web development</button>
                     <button className="btn">Web Applications</button>
                     <button className="btn">Web Design</button>
@@ -18,11 +26,11 @@ export function Header () {
                     <button className="btn">Front-End Development</button>
                     <button className="btn">Back-End Development</button>
                 </div>
-                <div className="buttons-middle">
+                <div className={`buttons-middle ${animate ? "animate" : ""}`}>
                     <button className="btn">Front-End Frameworks</button>
                     <button className="btn">Back-End Frameworks</button>
                 </div>
-                <div className="buttons-bottom">
+                <div className={`buttons-bottom ${animate ? "animate" : ""}`}>
                     <button className="btn">Computer Science</button>
                     <button className="btn">Software Development</button>
                     <button className="btn">Algorithms</button>
